dc32ff6fc3104270208533dd1310aa50e4a5a9e2
Narazaka <info@narazaka.net>
2024-04-02 19:30:21 +0900

    del apis

--- AvatarParametersUtil.cs
+++ AvatarParametersUtil.cs
@@ -8,84 +8,10 @@ using VRC.SDK3.Dynamics.PhysBone.Components;
 using System;
 using nadena.dev.ndmf;
 
-#if AVATAR_PARAMETERS_UTIL_HAS_MA
-using nadena.dev.modular_avatar.core;
-#endif
-
 namespace Narazaka.VRChat.AvatarParametersUtil
 {
     public static class AvatarParametersUtil
     {
-        class ParameterNameComparer : IEqualityComparer<VRCExpressionParameters.Parameter>
-        {
-            public bool Equals(VRCExpressionParameters.Parameter x, VRCExpressionParameters.Parameter y)
-            {
-                return x.name == y.name;
-            }
-
-            public int GetHashCode(VRCExpressionParameters.Parameter obj)
-            {
-                return obj.name.GetHashCode();
-            }
-        }
-
-        [Obsolete("Use nmdf ParameterInfo")]
-        public static IEnumerable<VRCExpressionParameters.Parameter> GetParameters(VRCAvatarDescriptor avatar, bool includeAnimators = false)
-        {
-            var parameters = Enumerable.Empty<VRCExpressionParameters.Parameter>();
-            if (avatar == null)
-            {
-                return parameters;
-            }
-
-            if (avatar.expressionParameters != null) parameters = parameters.Concat(avatar.expressionParameters.parameters);
-
-#if AVATAR_PARAMETERS_UTIL_HAS_MA
-            parameters = parameters.Concat(GetModularAvatarParameters(avatar));
-#endif
-            parameters = parameters.Concat(GetVRCPhysBoneParameters(avatar));
-            parameters = parameters.Concat(GetVRCContactReceiverParameters(avatar));
-
-            if (includeAnimators)
-            {
-#if UNITY_EDITOR
-                parameters = parameters.Concat(GetAvatarAnimatorControllerParameters(avatar));
-#if AVATAR_PARAMETERS_UTIL_HAS_MA
-                parameters = parameters.Concat(GetModularAvatarAnimatorControllerParameters(avatar));
-#endif
-#endif
-            }
-
-            return parameters.Where(p => !string.IsNullOrEmpty(p.name)).Distinct(new ParameterNameComparer());
-        }
-
-#if AVATAR_PARAMETERS_UTIL_HAS_MA
-        [Obsolete("Use nmdf ParameterInfo")]
-        public static IEnumerable<VRCExpressionParameters.Parameter> GetModularAvatarParameters(VRCAvatarDescriptor avatar)
-        {
-            var maParameters = avatar.GetAllComponentsInChildren<ModularAvatarParameters>();
-            return maParameters
-                .Where(map => map.parameters != null)
-                .SelectMany(maParameter => maParameter
-                    .parameters
-                    .Where(p => p.syncType != ParameterSyncType.NotSynced)
-                    .Select(p => new VRCExpressionParameters.Parameter
-                    {
-                        name = string.IsNullOrWhiteSpace(p.remapTo) ? p.nameOrPrefix : p.remapTo,
-                        valueType = p.syncType == ParameterSyncType.Bool ? VRCExpressionParameters.ValueType.Bool : p.syncType == ParameterSyncType.Int ? VRCExpressionParameters.ValueType.Int : VRCExpressionParameters.ValueType.Float,
-                        saved = p.saved,
-                        defaultValue = p.defaultValue,
-                        networkSynced = !p.localOnly,
-                    })
-                    );
-        }
-#endif
-
-        public static IEnumerable<VRCExpressionParameters.Parameter> GetVRCPhysBoneParameters(VRCAvatarDescriptor avatar)
-        {
-            return avatar.GetAllComponentsInChildren<VRCPhysBone>().SelectMany(ToVRCExpressionParametersParameters);
-        }
-
         public static IEnumerable<VRCExpressionParameters.Parameter> ToVRCExpressionParametersParameters(this VRCPhysBone physBone)
         {
             if (string.IsNullOrEmpty(physBone.parameter)) return Enumerable.Empty<VRCExpressionParameters.Parameter>();
@@ -128,15 +54,6 @@ namespace Narazaka.VRChat.AvatarParametersUtil
             };
         }
 
-        public static IEnumerable<VRCExpressionParameters.Parameter> GetVRCContactReceiverParameters(VRCAvatarDescriptor avatar)
-        {
-            var receivers = avatar.GetAllComponentsInChildren<VRCContactReceiver>();
-            foreach (var receiver in receivers)
-            {
-                yield return receiver.ToVRCExpressionParametersParameter();
-            }
-        }
-
         public static VRCExpressionParameters.Parameter ToVRCExpressionParametersParameter(this VRCContactReceiver receiver)
         {
             return new VRCExpressionParameters.Parameter
@@ -162,23 +79,6 @@ namespace Narazaka.VRChat.AvatarParametersUtil
         }
 
 #if UNITY_EDITOR
-        public static IEnumerable<VRCExpressionParameters.Parameter> GetAvatarAnimatorControllerParameters(VRCAvatarDescriptor avatar)
-        {
-            if (!avatar.customizeAnimationLayers) return Enumerable.Empty<VRCExpressionParameters.Parameter>();
-
-            var baseLayers = avatar.baseAnimationLayers.Where(l => !l.isDefault).Select(l => l.animatorController).Where(ac => ac != null);
-            var specialLayers = avatar.specialAnimationLayers.Where(l => !l.isDefault).Select(l => l.animatorController).Where(ac => ac != null);
-            return baseLayers.Concat(specialLayers).SelectMany(GetAnimatorControllerParameters);
-        }
-
-#if AVATAR_PARAMETERS_UTIL_HAS_MA
-        public static IEnumerable<VRCExpressionParameters.Parameter> GetModularAvatarAnimatorControllerParameters(VRCAvatarDescriptor avatar)
-        {
-            var maMergeAnimators = avatar.GetAllComponentsInChildren<ModularAvatarMergeAnimator>();
-            return maMergeAnimators.Select(ma => ma.animator).Where(ac => ac != null).SelectMany(GetAnimatorControllerParameters);
-        }
-#endif
-
         public static IEnumerable<VRCExpressionParameters.Parameter> GetAnimatorControllerParameters(RuntimeAnimatorController animatorController)
         {
             var controller = animatorController as UnityEditor.Animations.AnimatorController;
@@ -253,9 +153,5 @@ namespace Narazaka.VRChat.AvatarParametersUtil
             };
         }
 #endif
-        static T[] GetAllComponentsInChildren<T>(this Component component)
-        {
-            return component.GetComponentsInChildren<T>(true);
-        }
     }
 }
